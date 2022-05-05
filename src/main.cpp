31265c30c39b022c2e603d5762e6aefee57d5f6f
Abdolraouf-KIE <rahmaniabdolraouf@yahoo.com>
2022-05-05 12:14:41 +0800

    MQTT_works_alotOfPacketLoss

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,10 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
-#include <Adafruit_BME280.h>
-#include <Adafruit_Sensor.h>
 
 // Replace the next variables with your SSID/Password combination
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "2.4";
+const char* password = "296JBD82kK";
 
 // Add your MQTT Broker IP address, example:
 //const char* mqtt_server = "192.168.1.144";
@@ -21,39 +19,6 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
-int value = 0;
-
-//uncomment the following lines if you're using SPI
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-float temperature = 0;
-float humidity = 0;
-
-// LED Pin
-const int ledPin = 4;
-
-void setup() {
-  Serial.begin(115200);
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  //status = bme.begin();  
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-  pinMode(ledPin, OUTPUT);
-}
 
 void setup_wifi() {
   delay(10);
@@ -95,11 +60,11 @@ void callback(char* topic, byte* message, unsigned int length) {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
       Serial.println("on");
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
     }
     else if(messageTemp == "off"){
       Serial.println("off");
-      digitalWrite(ledPin, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
     }
   }
 }
@@ -122,36 +87,51 @@ void reconnect() {
     }
   }
 }
+
+void setup() {
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+int count=0;
+
 void loop() {
+  char tempString[]= "testing Temp";
   if (!client.connected()) {
     reconnect();
+    client.publish("esp32/temp", tempString);
   }
   client.loop();
 
   long now = millis();
   if (now - lastMsg > 5000) {
     lastMsg = now;
-    
-    // Temperature in Celsius
-    temperature = bme.readTemperature();   
-    // Uncomment the next line to set temperature in Fahrenheit 
-    // (and comment the previous temperature line)
-    //temperature = 1.8 * bme.readTemperature() + 32; // Temperature in Fahrenheit
-    
     // Convert the value to a char array
-    char tempString[8];
-    dtostrf(temperature, 1, 2, tempString);
+    // const char tempString[]= "testing Temp";
     Serial.print("Temperature: ");
+    sprintf(tempString, "%d", count);
     Serial.println(tempString);
-    client.publish("esp32/temperature", tempString);
 
-    humidity = bme.readHumidity();
+   if (!client.connected()){
+        reconnect();
+   } 
+    client.publish("esp32/temp", tempString);
     
     // Convert the value to a char array
-    char humString[8];
-    dtostrf(humidity, 1, 2, humString);
+    char humString[]= "Humidity Testing";
     Serial.print("Humidity: ");
+    sprintf(humString, "%d", count);
     Serial.println(humString);
-    client.publish("esp32/humidity", humString);
+
+  if (!client.connected()) {
+    reconnect();
+    }
+    client.publish("esp32/humid", humString);
+    count++;
   }
 }
