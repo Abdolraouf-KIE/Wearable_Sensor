ffd7e9c129bdf6c3aff563dbcc433135ec24e550
Abdolraouf-KIE <rahmaniabdolraouf@yahoo.com>
2022-05-16 20:31:00 +0800

    MAX variable glitch when MQTT is used.

--- src/main.cpp
+++ src/main.cpp
@@ -232,19 +232,24 @@ void loop()
         temp++;
 
         // send MQTT after temp reacehes 10 units (synonymous to after 10seconds)
-        if(temp==9){
+        if(temp==10){
             // Prepare JSON
             sprintf(JSONString, "{\"count\":[%d,%d,%d,%d,%d,%d,%d,%d,%d,%d], \"SPO2\":[%d,%d,%d,%d,%d,%d,%d,%d,%d,%d], \"HeartRate\":[%f,%f,%f,%f,%f,%f,%f,%f,%f,%f]}", CountAr[0],CountAr[1],CountAr[2],CountAr[3],CountAr[4],CountAr[5],CountAr[6],CountAr[7],CountAr[8],CountAr[9],SPO2Ar[0],SPO2Ar[1],SPO2Ar[2], SPO2Ar[3],SPO2Ar[4],SPO2Ar[5],SPO2Ar[6],SPO2Ar[7],SPO2Ar[8],SPO2Ar[9], HRAr[0],HRAr[1],HRAr[2],HRAr[3],HRAr[4],HRAr[5],HRAr[6],HRAr[7],HRAr[8],HRAr[9]);
             Serial.println(JSONString);
 
 
-            // while(!client.connected()){
-            //     reconnect();
-            // } 
+            while(!client.connected()){
+                reconnect();
+            } 
 
             //sending measurement via MQTT
-            // sendMQTT(JSONString, "esp32/humid");
+            sendMQTT(JSONString, "esp32/humid");
+            
+            //clear the variabels
             temp=0;
+            HRAr[10]={};
+            SPO2Ar[10]={};
+            CountAr[10]={};
         }
         
         count++;
