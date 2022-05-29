fa77eca2caaf106c0ae6a45597141a8cba37a64e
Abdolraouf-KIE <rahmaniabdolraouf@yahoo.com>
2022-05-29 23:15:17 +0800

    DS18B20 working fine

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,7 @@ void setup() {
 void loop() {
   sensors.requestTemperatures(); 
   float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
   Serial.print(temperatureC);
   Serial.println("ºC");
-  Serial.print(temperatureF);
-  Serial.println("ºF");
   delay(1000);
 }
